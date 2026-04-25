27c418652264a1835b5465013e2b222809b7318d
dolisha <dolishagandhi@gmail.com>
2026-04-25 12:25:56 +0530

    first commit

---
+++ Backend/model/user.js
@@ -0,0 +1,25 @@
+let express=require("express");
+let dotenv=require("dotenv");
+let cors=require("cors");
+let cookieParser=require("cookie-parser");
+
+dotenv.config();
+let app=express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+let { task } = require("./dbconnection");
+const PORT=process.env.PORT;
+app.listen(PORT,()=>
+{
+    console.log(`server is running on ${PORT}`);
+}
+)
