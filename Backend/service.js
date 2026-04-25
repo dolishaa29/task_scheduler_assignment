1a709664351684a134a604012b27d8c42780ed0b
dolisha <dolishagandhi@gmail.com>
2026-04-25 13:03:38 +0530

    first commit

--- Backend/service.js
+++ Backend/service.js
@@ -68,3 +68,48 @@ catch(err)
     res.status(500).json({msg:"internal server error"});
 }
 }
+
+exports.userprofile=async(req,res)=>
+{
+    const user = req.user;
+    return res.status(200).json({success: true,user})
+}
+
+exports.updateuser=async(req,res)=>
+{
+    const user=req.user;
+    let updatedData={
+        name:req.body.name,
+        contact:req.body.contact,
+        address:req.body.address,
+        gender:req.body.gender,
+        dob:req.body.dob,
+        bio:req.body.bio,
+    }
+    let updateduser= await rec.findOneAndUpdate({_id:user._id},updatedData,{new:true});
+    return res.status(200).json({ success: true, msg: "User profile updated successfully", user:updateduser });
+}
+
+exports.changepassword=async(req,res)=>
+{
+    try{
+    const user=req.user;
+    let oldpassword=req.body.oldpassword;
+    let newpassword=req.body.newpassword;
+    let data=await rec.findById(user._id);
+    let existing=data.password;
+    let pass=await bcrypt.compare(oldpassword,existing);
+    if(!pass)
+    {
+        return res.status(400).json({msg:"invalid old password"});
+    }
+    newpassword=await bcrypt.hash(newpassword,10);
+    await rec.findByIdAndUpdate(user._id,{password:newpassword});
+    return res.status(200).json({success:true, msg:"password updated successfully"});
+}
+catch(err)
+{
+    console.log(err);
+    res.status(500).json({msg:"internal server error"});
+}
+}
