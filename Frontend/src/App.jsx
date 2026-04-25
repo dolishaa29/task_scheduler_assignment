1749c1157c514d5a5d27f457881f334836dedc2c
dolisha <dolishagandhi@gmail.com>
2026-04-25 14:33:15 +0530

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import UserProfile from './pages/UserProfile'
+import Changepass from './pages/Changepass'
+import Dash from './pages/Dash'
 
 function App() {
 
   return (
-    <>
-     
-
-    </>
+   <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/userprofile' element={<UserProfile/>}/>
+      <Route path='/changepassword' element={<Changepass/>}/>
+      <Route path='/dash' element={<Dash/>}/>
+    </Routes>
+    </BrowserRouter>
+   </div>
   )
 }
 
---
+++ Frontend/src/pages/Changepass.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Changepass = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Changepass
